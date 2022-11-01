e0a9717b0512c575c428cd755fd6e6ecd54db3c8
Douglas <douglaspereira04@hotmail.com>
2022-11-01 17:07:12 -0300

    adds xenium harris michael map test

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <random>
 #include "test.cpp"
 
 int main(int argc, char *argv[]){
-    std::cout << "AAAA" << "\n";
+    
+    unsigned int n_threads = 4;
 
-    Test test(2,3,4);
+    unsigned long operations = (unsigned long) 1E7;
+
+    unsigned long pre_population = (unsigned long) 1E4;
+
+    double get_proportionn = 0.7;
+    double set_proportion = 0.2;
+    double delete_proportion = 0.1;
+
+    std::default_random_engine generator;
+    std::normal_distribution<double> distribution(10000, 200);
+    Test::HarrisMichaelMapRefCount16Test<std::normal_distribution<double>>(operations, n_threads, 
+        pre_population, get_proportionn, set_proportion, delete_proportion, distribution);
 }
--- test.cpp
+++ test.cpp
@@ -1,6 +1,10 @@
 #include <xenium/harris_michael_hash_map.hpp>
 #include <xenium/policy.hpp>
 #include <xenium/reclamation/lock_free_ref_count.hpp>
+#include <omp.h>
+#include <stdio.h>
+#include <random>
+#include <chrono>
 
 
 class Test{
@@ -15,7 +19,9 @@ public:
 	Test(unsigned long elapsedTime, unsigned long operations, unsigned int n_threads);
 	~Test();
 	
-	static Test HarrisMichaelMapTest(unsigned long operations, unsigned int n_threads, unsigned int buckets);
+	template <typename Distribution> static Test HarrisMichaelMapRefCount16Test(unsigned long operations, 
+		unsigned int n_threads, unsigned long prePopulation, double getProportion, 
+		double setProportion, double deleteProportion, Distribution distribution);
 };
 
 
@@ -27,11 +33,37 @@ Test::Test(unsigned long elapsedTime, unsigned long operations, unsigned int n_t
 
 Test::~Test(){};
 
-static Test HarrisMichaelMapRefCount10Test(unsigned long operations, unsigned int n_threads, unsigned int buckets, unsigned long prePopulation, double getProportion, double setProportion, double deleteProportion){
-	xenium::harris_michael_hash_map<std::string, int, xenium::policy::reclaimer<xenium::reclamation::lock_free_ref_count<>>, xenium::policy::buckets<10>> map;
-
+template <typename Distribution> Test Test::HarrisMichaelMapRefCount16Test(unsigned long operations, 
+	unsigned int n_threads, unsigned long prePopulation, double getProportion, 
+	double setProportion, double deleteProportion, Distribution distribution){
+	xenium::harris_michael_hash_map<int, int, xenium::policy::reclaimer<xenium::reclamation::lock_free_ref_count<>>, xenium::policy::buckets<2048>> map;
 	
+  	std::default_random_engine generator;
+	std::uniform_real_distribution<double> uniform(0.0,1.0);
+	
+	omp_set_num_threads(n_threads);
 
-	Test test_result(1U, 1U, 1U);
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+	#pragma omp parallel for schedule(static,1) private(generator, uniform)
+	for (size_t i = 0; i < operations; i++){
+		double chance = uniform(generator);
+		int key = (int)distribution(generator);
+
+		if (chance <= getProportion) {
+			// get
+			map.find(key);
+		} else if (chance <= (getProportion + setProportion)) {
+			// set
+			map.emplace(key, key);
+		} else {
+			// delete
+			map.erase(key);
+		}
+	}
+
+	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() << "[ns]" << std::endl;
+	Test test_result = Test(1U, 1U, 1U);
 	return test_result;	
 };
