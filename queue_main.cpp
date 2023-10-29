e3713314b75b257e5a952b4648fc8a8ddd696e92
Douglas Pereira Luiz <douglaspereira04@hotmail.com>
2023-10-29 20:33:15 -0300

    Adds queue tests

---
+++ queue_main.cpp
@@ -0,0 +1,221 @@
+#include <iostream>
+#include <random>
+#include "queue_test.cpp"
+#include <xenium/michael_scott_queue.hpp>
+#include <xenium/policy.hpp>
+#include <xenium/reclamation/lock_free_ref_count.hpp>
+#include <xenium/reclamation/hazard_pointer.hpp>
+#include <xenium/reclamation/hazard_eras.hpp>
+#include <xenium/reclamation/stamp_it.hpp>
+#include <xenium/reclamation/quiescent_state_based.hpp>
+#include <fstream>
+#include <string>
+#include <math.h>
+#include <array>
+
+#include <cds/gc/hp.h>
+#include <cds/gc/dhp.h>
+
+std::ofstream TEST_FILE;
+std::string TEST_FILE_NAME = "test.csv";
+
+void append_to_file(Test test, std::string text){
+
+    TEST_FILE.open (TEST_FILE_NAME, std::ios_base::app);
+    TEST_FILE << test.Structure() << text <<","
+        << test.ThreadAmount() << ","
+        << test.ElapsedTime() << "\n";
+    TEST_FILE.close();
+}
+void print_test(Test test, std::string text){
+
+    std::cout << test.Structure() << text <<",\t"
+        << test.ThreadAmount() << ",\t"
+        << test.ElapsedTime() << "\n";
+}
+
+template <typename Distribution, typename ...DistributionArgs>
+void test(
+    int thread_amount, 
+    long operations,
+    long pre_population,
+    long data_structure
+    ){
+    
+    Test test;
+    
+    switch (data_structure)
+    {
+    case 1:
+        test =  Test::STLQueue<uint32_t>(operations, thread_amount, pre_population);
+        print_test(test, "");
+        break;
+    case 2:
+        test =  Test::NikolaevQueue<uint32_t, 
+            xenium::reclamation::hazard_pointer<>>
+            (operations, thread_amount, pre_population);
+        print_test(test, "");
+        break;
+    case 3:
+        test =  Test::XeniumMSQueue<uint32_t, 
+            xenium::reclamation::hazard_pointer<>>
+            (operations, thread_amount, pre_population);
+        print_test(test, "");
+        break;
+    case 4:
+        test =  Test::CDSMSQueue<uint32_t, 
+            cds::gc::HP>
+            (operations, thread_amount, pre_population);
+        print_test(test, "");
+        break;
+    case 5:
+        test =  Test::CDSBasketQueue<uint32_t, 
+            cds::gc::HP>
+            (operations, thread_amount, pre_population);
+        print_test(test, "");
+        break;
+    case 6:
+        test =  Test::TBB<uint32_t>
+            (operations, thread_amount, pre_population);
+        print_test(test, "");
+        break;
+    case 7:
+        test =  Test::BoostQueue<uint32_t>
+            (operations, thread_amount, pre_population);
+        print_test(test, "");
+        break;
+    case 8:
+        test =  Test::STLQueue<std::array<uint32_t,1024>>(operations, thread_amount, pre_population);
+        print_test(test, "");
+        break;
+    case 9:
+        test =  Test::NikolaevQueue<std::array<uint32_t,1024>, 
+            xenium::reclamation::hazard_pointer<>>
+            (operations, thread_amount, pre_population);
+        print_test(test, "");
+        break;
+    case 10:
+        test =  Test::XeniumMSQueue<std::array<uint32_t,1024>, 
+            xenium::reclamation::hazard_pointer<>>
+            (operations, thread_amount, pre_population);
+        print_test(test, "");
+        break;
+    case 11:
+        test =  Test::CDSMSQueue<std::array<uint32_t,1024>, 
+            cds::gc::DHP>
+            (operations, thread_amount, pre_population);
+        print_test(test, "");
+        break;
+    case 12:
+        test =  Test::CDSBasketQueue<std::array<uint32_t,1024>, 
+            cds::gc::DHP>
+            (operations, thread_amount, pre_population);
+        print_test(test, "");
+        break;
+    case 13:
+        test =  Test::TBB<std::array<uint32_t,1024>>
+            (operations, thread_amount, pre_population);
+        print_test(test, "");
+        break;
+    case 14:
+        test =  Test::BoostQueue<std::array<uint32_t,1024>>
+            (operations, thread_amount, pre_population);
+        print_test(test, "");
+        break;
+    case 15:
+        test =  Test::STLQueueSPSC<uint32_t>(operations, pre_population);
+        print_test(test, "");
+        break;
+    case 16:
+        test =  Test::NikolaevSPSC<uint32_t, 
+            xenium::reclamation::hazard_pointer<>>
+            (operations, pre_population);
+        print_test(test, "");
+        break;
+    case 17:
+        test =  Test::XeniumMSSPSC<uint32_t, 
+            xenium::reclamation::hazard_pointer<>>
+            (operations, pre_population);
+        print_test(test, "");
+        break;
+    case 18:
+        test =  Test::CDSMSSPSC<uint32_t, 
+            cds::gc::HP>
+            (operations, pre_population);
+        print_test(test, "");
+        break;
+    case 19:
+        test =  Test::CDSBasketSPSC<uint32_t, 
+            cds::gc::HP>
+            (operations, pre_population);
+        print_test(test, "");
+        break;
+    case 20:
+        test =  Test::TBBSPSC<uint32_t>
+            (operations, pre_population);
+        print_test(test, "");
+        break;
+    case 21:
+        test =  Test::BoostSPSC<uint32_t>
+            (operations, pre_population);
+        print_test(test, "");
+        break;
+    case 22:
+        test =  Test::STLQueueSPSC<std::array<uint32_t,1024>>(operations, pre_population);
+        print_test(test, "");
+        break;
+    case 23:
+        test =  Test::NikolaevSPSC<std::array<uint32_t,1024>, 
+            xenium::reclamation::hazard_pointer<>>
+            (operations, pre_population);
+        print_test(test, "");
+        break;
+    case 24:
+        test =  Test::XeniumMSSPSC<std::array<uint32_t,1024>, 
+            xenium::reclamation::hazard_pointer<>>
+            (operations, pre_population);
+        print_test(test, "");
+        break;
+    case 25:
+        test =  Test::CDSMSSPSC<std::array<uint32_t,1024>, 
+            cds::gc::HP>
+            (operations, pre_population);
+        print_test(test, "");
+        break;
+    case 26:
+        test =  Test::CDSBasketSPSC<std::array<uint32_t,1024>, 
+            cds::gc::HP>
+            (operations, pre_population);
+        print_test(test, "");
+        break;
+    case 27:
+        test =  Test::TBBSPSC<std::array<uint32_t,1024>>
+            (operations, pre_population);
+        print_test(test, "");
+        break;
+    case 28:
+        test =  Test::BoostSPSC<std::array<uint32_t,1024>>
+            (operations, pre_population);
+        print_test(test, "");
+        break;
+    default:
+        break;
+    }
+}
+
+const size_t THREAD_AMOUNT = 1;
+const size_t OPERATIONS = 2;
+const size_t PRE_POPULATION = 3;
+const size_t DATA_STRUCTURE = 4;
+const size_t FILE_NAME = 5;
+
+int main(int argc, char *argv[]){
+    //TEST_FILE_NAME = argv[FILE_NAME];
+    
+    test<boost::random::uniform_int_distribution<uint32_t>, long, long>(
+        atoi(argv[THREAD_AMOUNT]),
+        atol(argv[OPERATIONS]), 
+        atol(argv[PRE_POPULATION]), 
+        atol(argv[DATA_STRUCTURE])
+    );
+}
--- queue_test.cpp
+++ queue_test.cpp
@@ -2,17 +2,28 @@
 #include <random>
 #include <chrono>
 #include <unordered_map>
-#include <tbb/concurrent_hash_map.h>
+#include <tbb/concurrent_queue.h>
 #include <xenium/michael_scott_queue.hpp>
+#include <xenium/nikolaev_queue.hpp>
 #include <string>
 #include <cds/init.h>
 #include <thread>
 #include <mutex>
+#include <semaphore.h>
+#include <queue>
+
+#include <boost/thread/sync_queue.hpp>
 
 #include <boost/random.hpp>
 
+#include <cds/container/msqueue.h>
+#include <cds/container/basket_queue.h>
+#include <cds/init.h>
 
 
+enum Library{
+	LIBCDS, STL, OTHER, BOOST
+};
 
 class Test{
 protected:
@@ -28,8 +39,60 @@ public:
 	Test(std::string structure, unsigned long elapsed_time, unsigned long operations, unsigned int thread_amount);
 	~Test();
 	
-template <typename ValueType, typename Reclaimer, bool OperationPairs, std::size_t GroupMaxSize, std::size_t Workload> 
-	static Test MichaelScottQueue(unsigned long operations, unsigned int thread_amount, 
+template <typename ValueType> 
+	static Test STLQueue(unsigned long operations, unsigned int thread_amount, 
+	unsigned long pre_population);
+
+template <typename ValueType> 
+	static Test STLQueueSPSC(unsigned long operations, 
+	unsigned long pre_population);
+
+template <typename ValueType> 
+	static Test BoostQueue(unsigned long operations, unsigned int thread_amount, 
+	unsigned long pre_population);
+
+template <typename ValueType> 
+	static Test BoostSPSC(unsigned long operations, 
+	unsigned long pre_population);
+
+template <typename ValueType, typename Reclaimer> 
+	static Test XeniumMSQueue(unsigned long operations, unsigned int thread_amount, 
+	unsigned long pre_population);
+
+template <typename ValueType, typename Reclaimer> 
+	static Test XeniumMSSPSC(unsigned long operations, 
+	unsigned long pre_population);
+
+template <typename ValueType, typename Reclaimer> 
+	static Test CDSMSQueue(unsigned long operations, unsigned int thread_amount, 
+	unsigned long pre_population);
+
+template <typename ValueType, typename Reclaimer> 
+	static Test CDSMSSPSC(unsigned long operations, 
+	unsigned long pre_population);
+
+template <typename ValueType, typename Reclaimer> 
+	static Test CDSBasketQueue(unsigned long operations, unsigned int thread_amount, 
+	unsigned long pre_population);
+
+template <typename ValueType, typename Reclaimer> 
+	static Test CDSBasketSPSC(unsigned long operations, 
+	unsigned long pre_population);
+
+template <typename ValueType, typename Reclaimer> 
+	static Test NikolaevQueue(unsigned long operations, unsigned int thread_amount, 
+	unsigned long pre_population);
+
+template <typename ValueType, typename Reclaimer> 
+	static Test NikolaevSPSC(unsigned long operations, 
+	unsigned long pre_population);
+
+template <typename ValueType> 
+	static Test TBB(unsigned long operations, unsigned int thread_amount, 
+	unsigned long pre_population);
+
+template <typename ValueType> 
+	static Test TBBSPSC(unsigned long operations, 
 	unsigned long pre_population);
 
 	std::string Structure(){
@@ -62,93 +125,172 @@ Test::Test(std::string structure, unsigned long elapsed_time, unsigned long oper
 
 Test::~Test(){};
 
-template <typename ValueType, typename Reclaimer, bool OperationPairs, std::size_t GroupMaxSize, std::size_t Workload> 
-	Test Test::MichaelScottQueue(unsigned long operations, unsigned int thread_amount, 
-	unsigned long pre_population){
+
+std::chrono::microseconds min_loop = std::chrono::microseconds(1);
+int loops = 50;
+
+template <typename ValueType, typename Structure, Library L> 
+Test spscTest(unsigned long operations, 
+unsigned long pre_population){
 
 	unsigned long elapsed_time;
-	std::thread thread[thread_amount];
+std::chrono::steady_clock::time_point begin;
+		
+	Structure queue;
+	sem_t sem;
+	std::mutex mtx;
+	sem_init(&sem, 0, 0);
 	
-	xenium::michael_scott_queue
-		<ValueType, xenium::policy::reclaimer<Reclaimer>> queue;
+	{//populacao inicial
+	
+		for (uint32_t i = 0; i < pre_population; i++){
+			ValueType v;
+			if constexpr(L == OTHER | L == LIBCDS | L == STL){
+				queue.push(v);
+			}
+			sem_post(&sem);
+		}
+	}
+
+	begin = std::chrono::steady_clock::now();
+
+	std::thread producer([&](){
+		if constexpr(L == LIBCDS){
+			cds::threading::Manager::attachThread();
+		}
+
+		for (size_t i = 0; i < operations; i++){
+
+			ValueType v;
+
+			if constexpr(L == OTHER | L == LIBCDS | L == BOOST){
+				queue.push(v);
+			} else if constexpr(L == STL){
+				mtx.lock();
+				queue.push(v);
+				mtx.unlock();
+
+			}
+			
+			sem_post(&sem);
+			for (size_t i = 0; i < loops; i++){}
+			
+		}
+		
+		if constexpr(L == LIBCDS){
+			cds::threading::Manager::detachThread();
+		}
+	});
+
+	std::thread consumer([&](){
+		if constexpr(L == LIBCDS){
+			cds::threading::Manager::attachThread();
+		}
+		for (size_t i = 0; i < operations; i++){
+
+			ValueType v;
+			sem_wait(&sem);
+			if constexpr(L == OTHER){
+				bool result = queue.try_pop(v);
+			} else if constexpr(L == STL){
+				mtx.lock();
+				v = queue.front();
+				if(!queue.empty()){
+					queue.pop();
+				}
+				mtx.unlock();
+			} else if constexpr(L == LIBCDS){
+				queue.pop(v);
+			} else if constexpr(L == BOOST){
+				auto result = queue.try_pull(v);
+			}
+
+			for (size_t i = 0; i < loops; i++){}
+			
+
+		}
+
+		if constexpr(L == LIBCDS){
+			cds::threading::Manager::detachThread();
+		}
+	});
+
+	producer.join();
+	consumer.join();
+
+	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+	elapsed_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
+	return Test("", elapsed_time, operations, 2);
+}
+
+template <typename ValueType, typename Structure, Library L> 
+Test testQueue(unsigned long operations, unsigned int thread_amount, 
+unsigned long pre_population){
+
+	unsigned long elapsed_time;
+	std::chrono::steady_clock::time_point begin;
+
+	std::thread thread[thread_amount];
+
+	Structure queue;
+	std::mutex mtx;
 
 	{//populacao inicial
 	
 		for (uint32_t i = 0; i < pre_population; i++){
 			ValueType v;
-			queue.push(v);
+			if constexpr(L == OTHER | L == LIBCDS | L == STL){
+				queue.push(v);
+			}
 		}
 	}
 
-	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+	begin = std::chrono::steady_clock::now();
 
 	for (size_t i = 0; i < thread_amount; i++){
 		thread[i] = std::thread([&](){
+
+			if constexpr(L == LIBCDS){
+				cds::threading::Manager::attachThread();
+			}
 			bool result;
-			if constexpr(!OperationPairs){
-				if constexpr(GroupMaxSize > 1){
-					boost::random::mt19937 chance_generator;
-					boost::random::mt19937 amount_generator;
-					boost::random::uniform_real_distribution<double> uniform_chance(0.0,1.0);
-					boost::random::uniform_int_distribution<uint32_t> uniform_amount(1,20);
 
-					for (size_t i = 0; i < operations/thread_amount; i++){
+			for (size_t i = 0; i < operations/thread_amount; i++){
 
-						uint32_t amount = uniform_amount(amount_generator);
-						double chance = uniform_chance(chance_generator);
+				ValueType v;
+				if constexpr(L == OTHER){
+					queue.push(v);
+					for (size_t i = 0; i < loops; i++){}
 
-						if (chance <= 0.5) {
-							for (uint32_t i = 0; i < amount; i++){
-								ValueType v;
-								queue.push(v);
-							}
-						} else {
-							ValueType v;
-							for (uint32_t i = 0; i < amount; i++){
-								result = queue.try_pop(v);
-							}
-						}
+					result = queue.try_pop(v);
+					for (size_t i = 0; i < loops; i++){}
+				} else if constexpr(L == STL){
+					mtx.lock();
+					queue.push(v);
+					mtx.unlock();
+					for (size_t i = 0; i < loops; i++){}
+					
+					mtx.lock();
+					v = queue.front();
+					if(!queue.empty()){
+						queue.pop();
 					}
-				} else{
-					boost::random::mt19937 chance_generator;
-					boost::random::uniform_real_distribution<double> uniform_chance(0.0,1.0);
+					mtx.unlock();
+					for (size_t i = 0; i < loops; i++){}
 
-					for (size_t i = 0; i < operations/thread_amount; i++){
+				} else if constexpr(L == LIBCDS){
+					queue.push(v);
+					for (size_t i = 0; i < loops; i++){}
 
-						double chance = uniform_chance(chance_generator);
-						ValueType v;
-						if (chance <= 0.5) {
-							queue.push(v);
-						} else {
-							result = queue.try_pop(v);
-						}
-					}
+					result = queue.pop(v);
+					for (size_t i = 0; i < loops; i++){}
 				}
-			}else {
-				if constexpr(GroupMaxSize > 1){
-					boost::random::mt19937 amount_generator;
-					boost::random::uniform_int_distribution<uint32_t> uniform_amount(1,20);
 
-					for (size_t i = 0; i < operations/thread_amount; i++){
+			}
 
-						uint32_t amount = uniform_amount(amount_generator);
-						for (uint32_t i = 0; i < amount; i++){
-							ValueType v;
-							queue.push(v);
-						}
-						amount = uniform_amount(amount_generator);
-						ValueType v;
-						for (uint32_t i = 0; i < amount; i++){
-							result = queue.try_pop(v);
-						}
-					}
-				} else{
-					for (size_t i = 0; i < operations/thread_amount; i++){
-						ValueType v;
-						queue.push(v);
-						result = queue.try_pop(v);
-					}
-				}
+
+			if constexpr(L == LIBCDS){
+				cds::threading::Manager::detachThread();
 			}
 		});
 	}
@@ -159,6 +301,160 @@ template <typename ValueType, typename Reclaimer, bool OperationPairs, std::size
 
 	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 	elapsed_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
-	return Test("MichaelScottQueue", elapsed_time, operations, thread_amount);
+	return Test("", elapsed_time, operations, thread_amount);
+}
+
+template <typename ValueType, typename Reclaimer> 
+	Test Test::NikolaevQueue(unsigned long operations, unsigned int thread_amount, 
+	unsigned long pre_population){
+	Test test = testQueue<ValueType, xenium::nikolaev_queue<ValueType, xenium::policy::reclaimer<Reclaimer>,  xenium::policy::entries_per_node<512*32>>, OTHER>(operations, thread_amount, pre_population);
+	test.structure = "LSCQ";
+	return test;
+};
+template <typename ValueType, typename Reclaimer> 
+	Test Test::NikolaevSPSC(unsigned long operations, 
+	unsigned long pre_population){
+	Test test = spscTest<ValueType, xenium::nikolaev_queue<ValueType, xenium::policy::reclaimer<Reclaimer>>, OTHER>(operations, pre_population);
+	test.structure = "LSCQ";
+	return test;
 };
 
+template <typename ValueType, typename Reclaimer> 
+	Test Test::XeniumMSQueue(unsigned long operations, unsigned int thread_amount, 
+	unsigned long pre_population){
+	Test test = testQueue<ValueType, xenium::michael_scott_queue<ValueType, xenium::policy::reclaimer<Reclaimer>>, OTHER>(operations, thread_amount, pre_population);
+	test.structure = "XMSQ";
+	return test;
+};
+template <typename ValueType, typename Reclaimer> 
+	Test Test::XeniumMSSPSC(unsigned long operations, 
+	unsigned long pre_population){
+	Test test = spscTest<ValueType, xenium::michael_scott_queue<ValueType, xenium::policy::reclaimer<Reclaimer>>, OTHER>(operations, pre_population);
+	test.structure = "XMSQ";
+	return test;
+};
+
+		
+struct MSTraits: public cds::container::msqueue::traits {
+	typedef cds::intrusive::msqueue::stat<> stat;
+	typedef cds::atomicity::item_counter item_counter;
+};
+
+template <typename ValueType, typename Reclaimer> 
+	Test Test::CDSMSQueue(unsigned long operations, unsigned int thread_amount, 
+	unsigned long pre_population){
+	typedef cds::container::MSQueue<Reclaimer, ValueType, MSTraits > queue;
+	Reclaimer gc;
+	cds::Initialize();
+	cds::threading::Manager::attachThread();
+
+	Test test = testQueue<ValueType, queue, LIBCDS>(operations, thread_amount, pre_population);
+
+	cds::Terminate();
+	test.structure = "CDSMSQ";
+	return test;
+};
+
+template <typename ValueType, typename Reclaimer> 
+	Test Test::CDSMSSPSC(unsigned long operations, 
+	unsigned long pre_population){
+	typedef cds::container::MSQueue<Reclaimer, ValueType, MSTraits > queue;
+
+	Reclaimer gc;
+	cds::Initialize();
+	cds::threading::Manager::attachThread();
+
+	Test test = spscTest<ValueType, queue, LIBCDS>(operations, pre_population);
+
+	cds::Terminate();
+	test.structure = "CDSMSQ";
+	return test;
+};
+
+
+struct BasketTraits: public cds::container::basket_queue::traits {
+	typedef cds::intrusive::basket_queue::stat<> stat;
+	typedef cds::atomicity::item_counter item_counter;
+};
+
+template <typename ValueType, typename Reclaimer> 
+	Test Test::CDSBasketQueue(unsigned long operations, unsigned int thread_amount, 
+	unsigned long pre_population){
+	typedef cds::container::BasketQueue<Reclaimer, ValueType, BasketTraits > queue;
+	Reclaimer gc;
+	cds::Initialize();
+	cds::threading::Manager::attachThread();
+
+	Test test = testQueue<ValueType, queue, LIBCDS>(operations, thread_amount, pre_population);
+
+	cds::Terminate();
+	test.structure = "BsktQ";
+	return test;
+};
+
+template <typename ValueType, typename Reclaimer> 
+	Test Test::CDSBasketSPSC(unsigned long operations, 
+	unsigned long pre_population){
+	typedef cds::container::BasketQueue<Reclaimer, ValueType, BasketTraits > queue;
+
+	Reclaimer gc;
+	cds::Initialize();
+	cds::threading::Manager::attachThread();
+
+	Test test = spscTest<ValueType, queue, LIBCDS>(operations, pre_population);
+
+	cds::Terminate();
+	test.structure = "BsktQ";
+	return test;
+};
+
+template <typename ValueType> 
+	Test Test::TBB(unsigned long operations, unsigned int thread_amount, 
+	unsigned long pre_population){
+
+	Test test = testQueue<ValueType, tbb::concurrent_queue<ValueType>, OTHER>(operations, thread_amount, pre_population);
+	test.structure = "TBBQ";
+	return test;
+};
+
+template <typename ValueType> 
+	Test Test::TBBSPSC(unsigned long operations, 
+	unsigned long pre_population){
+	Test test = spscTest<ValueType, tbb::concurrent_queue<ValueType>, OTHER>(operations, pre_population);
+	test.structure = "TBBQ";
+	return test;
+};
+
+
+
+template <typename ValueType> 
+	Test Test::STLQueue(unsigned long operations, unsigned int thread_amount, 
+		unsigned long pre_population){
+	Test test = testQueue<ValueType, std::queue<ValueType>, STL>(operations, thread_amount, pre_population);
+	test.structure = "STLQ";
+	return test;
+}
+
+template <typename ValueType> 
+	Test Test::STLQueueSPSC(unsigned long operations, 
+		unsigned long pre_population){
+	Test test = spscTest<ValueType, std::queue<ValueType>, STL>(operations, pre_population);
+	test.structure = "STLQ";
+	return test;
+}
+
+template <typename ValueType> 
+	Test Test::BoostQueue(unsigned long operations, unsigned int thread_amount, 
+		unsigned long pre_population){
+	Test test = testQueue<ValueType, boost::sync_queue<ValueType>, BOOST>(operations,thread_amount, pre_population);
+	test.structure = "BoostQ";
+	return test;
+}
+
+template <typename ValueType> 
+	Test Test::BoostSPSC(unsigned long operations, 
+		unsigned long pre_population){
+	Test test = spscTest<ValueType, boost::sync_queue<ValueType>, BOOST>(operations, pre_population);
+	test.structure = "BoostQ";
+	return test;
+}
